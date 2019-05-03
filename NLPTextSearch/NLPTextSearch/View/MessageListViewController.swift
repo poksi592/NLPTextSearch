2da2132cdada2dc94489a919964a0a94298ce5d1
Mladen Despotovic <mladen.despotovic@icloud.com>
2019-05-03 21:24:42 +0200

    Added FetchedResultsController

--- NLPTextSearch/NLPTextSearch/View/MessageListViewController.swift
+++ NLPTextSearch/NLPTextSearch/View/MessageListViewController.swift
@@ -12,8 +12,9 @@ import CoreData
 
 class MessageListViewController: UITableViewController {
     
-    var numberOfRows: Int = 0
+    var numberOfRows: Int?
     private(set) var messages = [Message]()
+    private(set) var fetchedResultsController: NSFetchedResultsController<Message>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +24,15 @@ class MessageListViewController: UITableViewController {
         let managedObjectContext = Persistence.shared.persistentContainer.viewContext
         let fetchRequest: NSFetchRequest<Message> = Message.fetchRequest()
         fetchRequest.fetchLimit = 100
-        messages = try! managedObjectContext.fetch(fetchRequest)
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "messageID", ascending: true)]
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                              managedObjectContext: managedObjectContext,
+                                                              sectionNameKeyPath: nil,
+                                                              cacheName: nil)
+        try! fetchedResultsController?.performFetch()
+        numberOfRows = fetchedResultsController?.fetchedObjects?.count
         
         tableView.reloadData()
-        
-        print(messages)
     }
 }
 
@@ -38,14 +43,19 @@ extension MessageListViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfRows > 100 ? 100 : numberOfRows
+        return numberOfRows! > 100 ? 100 : numberOfRows!
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "MessageListCell", for: indexPath) as! MessageListCell
-        cell.subjectLabel?.text = messages[indexPath.row].subject
-        cell.contentLabel?.text = messages[indexPath.row].content
+        
+        guard let message = fetchedResultsController?.object(at: indexPath) else {
+            fatalError("Attempt to configure cell without a managed object")
+        }
+        
+        cell.subjectLabel?.text = message.subject
+        cell.contentLabel?.text = message.content
         return cell
     }
 }
