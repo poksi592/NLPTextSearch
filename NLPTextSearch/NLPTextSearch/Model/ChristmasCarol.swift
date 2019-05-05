cafb4e72a9823b3d3154b7eddb8951fe200d1300
Mladen Despotovic <mladen.despotovic@icloud.com>
2019-05-05 11:55:47 +0200

    Build tokenised dictionary

--- NLPTextSearch/NLPTextSearch/Model/ChristmasCarol.swift
+++ NLPTextSearch/NLPTextSearch/Model/ChristmasCarol.swift
@@ -11,14 +11,21 @@ import Foundation
 class ChristmasCarolMessages {
     
     static let shared = ChristmasCarolMessages()
-    let tokenMessageDictionary: [String : [Int]]
+    var tokenMessageDictionary: [String : Set<Int>]
     let allChristmasCarolWords: [String]
+    let tokenisation = Tokenisation()
     
     init() {
         
         let christmasCarolText = ChristmasCarolMessages.christmasCarol()
-        let tokenisation = Tokenisation()
-        let dictionary = Dictionary(uniqueKeysWithValues: zip(tokenisation.collectLinguisticTokens(from: christmasCarolText), [[Int]]()))
+        let linguisticTokens = tokenisation.collectLinguisticTokens(from: christmasCarolText)
+        let dictionary = linguisticTokens.reduce([String : Set<Int>](), { dict, token in
+            
+            var varDict = dict
+            varDict[token] = Set<Int>()
+            return varDict
+        })
+        
         self.tokenMessageDictionary = dictionary
         self.allChristmasCarolWords = tokenisation.collectTokens(from: christmasCarolText)
     }
@@ -30,4 +37,63 @@ class ChristmasCarolMessages {
         return try! String(contentsOf: url)
     }
     
+}
+
+extension ChristmasCarolMessages {
+    
+    func generateMessages(numberOfMessages: Int, numberOfWordsInMessage: Int) {
+        
+        let christmasCarol = ChristmasCarolMessages.shared
+        let managedObjectContext = Persistence.shared.persistentContainer.viewContext
+        
+        // MARK: Delete all data
+        let allMessages = try! managedObjectContext.fetch(Message.fetchRequest()) as! [Message]
+        managedObjectContext.performAndWait {
+            for message in allMessages {
+                managedObjectContext.delete(message)
+            }
+        }
+        Persistence.shared.saveContext()
+        
+        for messageNumber in 1...numberOfMessages {
+            
+            var message = ""
+            for _ in 1...numberOfWordsInMessage {
+                
+                let randomWordIndex = Int.random(in: 0..<christmasCarol.allChristmasCarolWords.count)
+                message.append(christmasCarol.allChristmasCarolWords[randomWordIndex])
+                message.append(" ")
+            }
+            
+            var subject = ""
+            for _ in 1...5 {
+                
+                let randomWordIndex = Int.random(in: 0..<christmasCarol.allChristmasCarolWords.count)
+                subject.append(christmasCarol.allChristmasCarolWords[randomWordIndex])
+                subject.append(" ")
+            }
+            
+            let messageObject = Message(context: managedObjectContext)
+            messageObject.messageID = Int32(messageNumber)
+            messageObject.subject = "#" + String(messageNumber) + " " + subject
+            messageObject.content = message
+            
+            tokenizeMessage(messageObject)
+        }
+        Persistence.shared.saveContext()
+    }
+    
+    func tokenizeMessage(_ message: Message) {
+        
+        let subjectTokens = tokenisation.collectLinguisticTokens(from: message.subject!)
+        let contentTokens = tokenisation.collectLinguisticTokens(from: message.content!)
+        let tokenSet = Set(subjectTokens).union(Set(contentTokens))
+        
+        tokenSet.forEach { token in
+
+            var messageNumbers = tokenMessageDictionary[token]
+            messageNumbers?.insert(Int(message.messageID))
+            tokenMessageDictionary[token] = messageNumbers
+        }
+    }
 }
--- NLPTextSearch/NLPTextSearch/View/GenerateMessagesViewController.swift
+++ NLPTextSearch/NLPTextSearch/View/GenerateMessagesViewController.swift
@@ -23,44 +23,10 @@ class GenerateMessagesViewController: UIViewController {
         
         let numberOfMessages = Int(noOfMessages.text!)
         let numberOfWordsInMessage = Int(noOfWordsInMessage.text!)
-        
         let christmasCarol = ChristmasCarolMessages.shared
-        let managedObjectContext = Persistence.shared.persistentContainer.viewContext
-        
-        // MARK: Delete all data
-        let allMessages = try! managedObjectContext.fetch(Message.fetchRequest()) as! [Message]
-        managedObjectContext.performAndWait {
-            for message in allMessages {
-                managedObjectContext.delete(message)
-            }
-        }
-        Persistence.shared.saveContext()
-        
-        for messageNumber in 1...numberOfMessages! {
-            
-            var message = ""
-            for _ in 1...numberOfWordsInMessage! {
-                
-                let randomWordIndex = Int.random(in: 0..<christmasCarol.allChristmasCarolWords.count)
-                message.append(christmasCarol.allChristmasCarolWords[randomWordIndex])
-                message.append(" ")
-            }
-
-            var subject = ""
-            for _ in 1...5 {
-                
-                let randomWordIndex = Int.random(in: 0..<christmasCarol.allChristmasCarolWords.count)
-                subject.append(christmasCarol.allChristmasCarolWords[randomWordIndex])
-                subject.append(" ")
-            }
-            
-            let messageObject = Message(context: managedObjectContext)
-            messageObject.messageID = Int32(messageNumber)
-            messageObject.subject = "#" + String(messageNumber) + " " + subject
-            messageObject.content = message
-            
-        }
-        Persistence.shared.saveContext()
+       
+        christmasCarol.generateMessages(numberOfMessages: numberOfMessages!,
+                                        numberOfWordsInMessage: numberOfWordsInMessage!)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
